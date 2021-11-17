6d273dc0ef74436168b62d3600664155b0a6f0c7
cosrick <rick830620@gmail.com>
2021-11-17 22:49:35 +0800

    CDK Deploy function to lambda, and API gateway

--- .projenrc.js
+++ .projenrc.js
@@ -6,6 +6,7 @@ const project = new AwsCdkTypeScriptApp({
 
   cdkDependencies: [
     '@aws-cdk/aws-lambda-nodejs',
+    '@aws-cdk/aws-iam',
   ], /* Which AWS CDK modules (those that start with "@aws-cdk/") this app uses. */
   deps: [
     '@softchef/cdk-restapi',
@@ -13,10 +14,12 @@ const project = new AwsCdkTypeScriptApp({
     'jsonwebtoken',
     '@aws-sdk/client-ssm',
     'jwt-decode',
+    'pem-jwk',
   ], /* Runtime dependencies of this module. */
   // description: undefined,      /* The description is just a string that helps people understand the purpose of the package. */
   devDeps: [
     '@types/jsonwebtoken',
+    '@types/pem-jwk',
   ], /* Build dependencies for this module. */
   // packageName: undefined,      /* The "name" in package.json. */
   // release: undefined,          /* Add release management to this project. */
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import path from 'path';
+import * as iam from '@aws-cdk/aws-iam';
+import * as lambda from '@aws-cdk/aws-lambda-nodejs';
 import { App, Construct, Stack, StackProps } from '@aws-cdk/core';
 import { RestApi, HttpMethod } from '@softchef/cdk-restapi';
 
@@ -8,11 +10,106 @@ export class MyStack extends Stack {
     super(scope, id, props);
 
     // define resources here...
-    new RestApi(this, 'TwitterOpenIdApi', {
-      resources: [],
+    new RestApi(this, 'OpenIdApi', {
+      resources: [
+        {
+          path: '/oauth2/token',
+          httpMethod: HttpMethod.POST,
+          lambdaFunction: this.createOauthTokenLambda(),
+        },
+        {
+          path: '/oauth2/authorize',
+          httpMethod: HttpMethod.GET,
+          lambdaFunction: new lambda.NodejsFunction(this, 'OpenIdAuthorize', {
+            entry: `${LAMBDA_ASSETS_PATH}/oauth2/authorize.ts`,
+          }),
+        },
+        {
+          path: '/oauth2/userInfo',
+          httpMethod: HttpMethod.GET,
+          lambdaFunction: new lambda.NodejsFunction(this, 'OpenIdUserInfo', {
+            entry: `${LAMBDA_ASSETS_PATH}/oauth2/user-info.ts`,
+          }),
+        },
+        {
+          path: '/.well-known/jwks.json',
+          httpMethod: HttpMethod.GET,
+          lambdaFunction: this.createJwksLambda(),
+        },
+        {
+          path: '/.well-known/openid-configuration',
+          httpMethod: HttpMethod.GET,
+          lambdaFunction: new lambda.NodejsFunction(this, 'OpenIdConfiguration', {
+            entry: `${LAMBDA_ASSETS_PATH}/.well-known/openid-configuration.ts`,
+          }),
+        },
+      ],
       enableCors: true,
     });
   }
+
+  private createOauthTokenLambda(): lambda.NodejsFunction {
+    // Deploy lambda function
+    const oauth2TokenLambda = new lambda.NodejsFunction(this, 'TwitterOpenIdToken', {
+      entry: `${LAMBDA_ASSETS_PATH}/oauth2/token.ts`,
+      bundling: {
+        nodeModules: [
+          '@aws-sdk/client-ssm',
+          'jsonwebtoken',
+          'url',
+        ],
+      },
+    });
+
+    // Assign necessary role policy for permission
+    oauth2TokenLambda.role?.attachInlinePolicy(
+      new iam.Policy(this, 'AccessSSMFromToken', {
+        statements: [
+          new iam.PolicyStatement({
+            actions: [
+              'ssm:GetParameter',
+            ],
+            resources: [
+              '<SSM Arn>',
+            ],
+          }),
+        ],
+      }),
+    );
+
+    return oauth2TokenLambda;
+  }
+
+  private createJwksLambda(): lambda.NodejsFunction {
+    const jwksLambda = new lambda.NodejsFunction(this, 'TwitterOpenIdJwks', {
+      entry: `${LAMBDA_ASSETS_PATH}/.well-known/jwks.ts`,
+      bundling: {
+        nodeModules: [
+          'pem-jwk',
+          '@aws-sdk/client-ssm',
+        ],
+      },
+    });
+
+    // Assign necessary role policy for permission
+    jwksLambda.role?.attachInlinePolicy(
+      new iam.Policy(this, 'AccessSSMFromToken', {
+        statements: [
+          new iam.PolicyStatement({
+            actions: [
+              'ssm:GetParameter',
+            ],
+            resources: [
+              '<SSM Arn>',
+            ],
+          }),
+        ],
+      }),
+    );
+
+    return jwksLambda;
+  }
+
 }
 
 // for development, use account/region from cdk cli
